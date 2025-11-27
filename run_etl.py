8c2460ec824f0e70bcd72143ff686f21e26f849f
DoodlesHuman <amosrr@gmail.com>
2025-11-27 12:20:43 +0100

    Load Section Debug

--- run_etl.py
+++ run_etl.py
@@ -90,39 +90,7 @@ def transform_data(raw_data_list):
     df = pd.DataFrame(transformed)
     print(f"Transformed {len(df)} rows into a DataFrame.")
     return df
-"""
-# Load
-def load_data(new_data, file_path=DATA_FILE_PATH):
 
-    if new_data.empty:
-        print("No new data to load.")
-        return
-
-    print(f"Loading data to {file_path}...")
-    try:
-        existing_data = pd.read_csv(file_path)
-        combined_data = pd.concat([existing_data, new_data], ignore_index=True)
-        
-        # De-duplication, as the 5-day forecast will overlap each day
-        # We keep the latest fetched forecast for any given city/time
-        combined_data = combined_data.drop_duplicates(
-            subset=['city', 'forecast_time'], 
-            keep='last'
-        )
-        
-    except FileNotFoundError:
-        print("File not found. Creating new file.")
-        combined_data = new_data
-    
-    # Ensure directory exists
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    
-    # Sort data for readability
-    combined_data.sort_values(by=['city', 'forecast_time'], inplace=True)
-    
-    combined_data.to_csv(file_path, index=False)
-    print(f"Successfully saved data to {file_path}.")
-"""
 # --- LOAD (Debug Version) ---
 def load_data(new_data, file_path=DATA_FILE_PATH):
     """
